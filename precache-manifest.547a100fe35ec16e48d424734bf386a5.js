3e76a4e00bd18101f3e4663f2d44bc745f016c23
Ya_Myn <ricky.month@gmail.com>
2020-04-08 15:53:32 +0300

    Updates

---
+++ precache-manifest.547a100fe35ec16e48d424734bf386a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5f52ef5c861c172052d47a5ea930ed",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "a9f4d490c3821ad1f441",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.82c89fd9.chunk.css"
+  },
+  {
+    "revision": "53fd901fbcec572f1b64",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.5696cb24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.5696cb24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9f4d490c3821ad1f441",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.f2c4464a.chunk.js"
+  },
+  {
+    "revision": "c5c27286cb70be1a5d87",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.8e26d498.js"
+  }
+]);
